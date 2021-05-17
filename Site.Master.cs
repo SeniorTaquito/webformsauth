54ee6e08d89403762022cfc99bb8fbce7445d44b
Jessica Perez <dcohen626@gmail.com>
2021-05-17 18:12:02 -0400

    initial commit

---
+++ Site.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace WebFormsAuth
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            HttpContext.Current.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/"
+            },
+           "Auth0");
+        }
+
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,90 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNet.Identity;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Security.Claims;
+using Microsoft.Owin.Host.SystemWeb;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebFormsAuth.Startup))]
+
+namespace WebFormsAuth
+{
+    public class Startup
+    {
+        private readonly string _clientId = ConfigurationManager.AppSettings["ClientId"];
+
+        private readonly string _redirectUri = ConfigurationManager.AppSettings["RedirectUri"];
+        private readonly string _authority = ConfigurationManager.AppSettings["Domain"];
+        private readonly string _clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+        private readonly string _postLogoutRedirectUri = ConfigurationManager.AppSettings["PostLogout"];
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+                LoginPath = new PathString("/Account/Login"),
+                CookieSameSite = SameSiteMode.Lax,
+                
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationType = "Auth0",
+                ClientId = _clientId,
+                ClientSecret = _clientSecret,
+                Authority = _authority,
+                RedirectUri = _redirectUri,
+                PostLogoutRedirectUri = _postLogoutRedirectUri,
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                Scope = OpenIdConnectScope.OpenIdProfile,              
+                UseTokenLifetime = false,
+                RedeemCode = true,
+                SaveTokens = true,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name"
+                },
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    RedirectToIdentityProvider = notification =>
+                    {
+                        if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
+                        {
+                            var logoutUri = $"{_authority}/v2/logout?client_id={_clientId}";
+
+                            var postLogoutUri = notification.ProtocolMessage.PostLogoutRedirectUri;
+                            if (!string.IsNullOrEmpty(postLogoutUri))
+                            {
+                                if (postLogoutUri.StartsWith("/"))
+                                {
+                                    // transform to absolute
+                                    var request = notification.Request;
+                                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                                }
+                                logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                            }
+
+                            notification.Response.Redirect(logoutUri);
+                            notification.HandleResponse();
+                        }
+                        return Task.FromResult(0);
+                    }
+                }                
+            });
+
+        }
+    }
+}
